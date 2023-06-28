1e8a7c644a75f908997437ac6ea46af3f0eee8fc
Narayan Bandodker <narayanband1356@gmail.com>
2023-06-28 23:44:28 +0530

    Monkey-patch to fix bug in package

--- langcorn_app.py
+++ langcorn_app.py
@@ -2,6 +2,17 @@
 from fastapi import FastAPI
 
 from langcorn import create_service
+from langcorn.server import api as lcorn_api
+
+# Monkey-patching to fix bug of input/output keys (till fix is updated)
+def _fixed_derive_fields(language_app):
+    if hasattr(language_app, "input_variables"):
+        return language_app.input_variables, language_app.output_variables
+    elif hasattr(language_app, "prompt"):
+        return language_app.prompt.input_variables, [language_app.output_key]
+    return language_app.input_keys, language_app.output_keys
+
+setattr(lcorn_api, 'derive_fields', _fixed_derive_fields)
 
 
 app: FastAPI = create_service("genesis.chat_chain:agent_chain")
